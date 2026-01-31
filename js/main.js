fa27e9da558a8b9a5056120ecf479df112d6688d
jerash9 <jerash.fuentevilla@alumnos.esne.es>
2026-01-31 12:06:57 +0100

    mediakery + leer mas +arreglar cositas

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
 
   AOS.init();
+const textoMini = ["Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque aliquid optio"]
+  const texto = ["Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque aliquid optio, necessitatibus nesciunt iste incidunt ipsa, sequi animi laudantium quas at, modi repellendus cupiditate atque impedit aperiam vel nemo vero."]
+
+let siu = 0;
+let visible = false;
+
+const boton = document.getElementById("botton-read-more")
+const textoo = document.getElementById("textoo")
+
+boton.addEventListener('click', () => {
+    if (!visible) {
+        textoo.textContent = texto[siu];
+        textoo.style.display = "block";
+        visible = true;
+        siu++; 
+        if (siu >= texto.length) siu = 0; 
+    } else {
+        textoo.textContent = textoMini[siu];
+        visible = false;
+    }
+});
