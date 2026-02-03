d1c5fa1e7ed06fd2a267aa6253905b73696f05f0
jerash9 <jerash.fuentevilla@alumnos.esne.es>
2026-02-03 11:45:29 +0100

    Index Terminado

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 
   AOS.init();
 
+
+
+ 
+  const hamburger = document.querySelector('.hamburger');
+  const mobileMenu = document.querySelector('.mobile-menu');
+
+  hamburger.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+  });
+
