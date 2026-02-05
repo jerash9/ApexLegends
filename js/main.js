879fc6d751afc4eac6e8ca990bb42c588d2f505e
StargazerVega <roberto.ferrerasmoreno@alumnos.esne.es>
2026-02-05 12:58:20 +0100

    Weapon Modal windows added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,54 @@
 AOS.init();
 
+
+
+    //VENTANAS MODALES DE LAS ARMAS
+
+    function openWeaponWindow(weapon_id) {
+        console.log("hola")
+        let weaponWindow = document.querySelector("#weaponWindow" + weapon_id);
+        weaponWindow.classList.add("show-modal");   
+    }
+
+    // Función para cerrar la ventana modal 
+    function closeWeaponWindow(weapon_id) {
+        let weaponWindow = document.querySelector("#weaponWindow" + weapon_id);
+        weaponWindow.classList.remove("show-modal");  
+    }
+
+    // Array de botones de abrir
+    let weaponsBtns = document.querySelectorAll(".weapon");
+
+    for (let k = 0; k < weaponsBtns.length; k++) {
+        console.log("weaponsBtns"+[k])
+        weaponsBtns[k].addEventListener("click", function() {
+            openWeaponWindow(this.dataset.weapon);
+        });
+
+    }
+
+    // Array de botones de cerrar 
+
+    let weaponXClose = document.querySelectorAll(".close");
+
+    for (let m = 0; m < weaponXClose.length; m++) {
+        console.log("weaponXClose"+[m])
+        weaponXClose[m].addEventListener("click", function() {
+            closeWeaponWindow(this.dataset.closemodal);
+        })
+    }
+
+    let weaponsWindows = document.querySelectorAll(".weapon-window");
+
+    for (let n = 0; n < weaponsWindows.length; n++) {
+        console.log("weaponsWindows"+[n])
+        window.addEventListener("click", function(event){
+            if (event.target == weaponsWindows[n]) {
+                closeWeaponWindow(weaponsWindows[n].dataset.modalnumber);
+            }
+        })
+    }
+
 $(document).ready(function(){
       
     $(".charactertext").hide();
@@ -11,68 +60,66 @@ $(document).ready(function(){
       
       
       
-      // Botones de cambio de personaje
+    // Botones de cambio de personaje
       
-      function changeChara(chara_id) {
-          //hide currently visible
-          $(".visible").hide();
-          $(".visible").removeClass("visible");
-          //show chosen character
-          $("#p"+chara_id).show()
-          $("#p"+chara_id).addClass("visible");
-          $("#t"+chara_id).show()
-          $("#t"+chara_id).addClass("visible");
-        };
+    function changeChara(chara_id) {
+        //hide currently visible
+        $(".visible").hide();
+        $(".visible").removeClass("visible");
+        //show chosen character
+        $("#p"+chara_id).show()
+        $("#p"+chara_id).addClass("visible");
+        $("#t"+chara_id).show()
+        $("#t"+chara_id).addClass("visible");
+    };
         
-        let charaBtns = document.querySelectorAll(".card");
+    let charaBtns = document.querySelectorAll(".card");
         
-        for (let j = 0; j < charaBtns.length; j++) {
-            charaBtns[j].addEventListener("click",function(){
-                changeChara(this.dataset.chara);
-            });
+    for (let j = 0; j < charaBtns.length; j++) {
+        charaBtns[j].addEventListener("click",function(){
+            changeChara(this.dataset.chara);
+        });
             
         };
         
-        // Botones de cambio de mapa
+    // Botones de cambio de mapa
         
-        function changeMap(map_id) {
-            document.getElementById("bg").style.backgroundImage = "url('assets/mapas/Map_"+map_id+".webp')";
-        };
+    function changeMap(map_id) {
+        document.getElementById("bg").style.backgroundImage = "url('assets/mapas/Map_"+map_id+".webp')";
+    };
         
-        let mapsBtns = document.querySelectorAll(".map");
+    let mapsBtns = document.querySelectorAll(".map");
         
-        for (let i = 0; i < mapsBtns.length; i++) {
-            mapsBtns[i].addEventListener("click",function(){
-                changeMap(this.dataset.map);
-            });
-            
-        };
+    for (let i = 0; i < mapsBtns.length; i++) {
+        mapsBtns[i].addEventListener("click",function(){
+            changeMap(this.dataset.map);
+        });            
+    };
         
-        // Menu hamburguesa
-
-        
-        // mapa inicial
-        const layers = document.querySelectorAll(".map-layer");
-        let currentLayer = 0;
-
-        function changeMap(map_id) {
+    
+    // mapa inicial
+    const layers = document.querySelectorAll(".map-layer");
+    let currentLayer = 0;
+    
+    function changeMap(map_id) {
         const nextLayer = (currentLayer + 1) % 2;
-
+        
         layers[nextLayer].style.backgroundImage ="url('assets/mapas/Map_" + map_id + ".webp')";
-
+        
         layers[nextLayer].classList.add("active");
         layers[currentLayer].classList.remove("active");
-
+        
         currentLayer = nextLayer;
-        }   
+    }   
+    
+    // Menu hamburguesa
 
-
-        const hamburger = document.querySelector('.hamburger');
-        const mobileMenu = document.querySelector('.mobile-menu');
+    const hamburger = document.querySelector('.hamburger');
+    const mobileMenu = document.querySelector('.mobile-menu');
       
-        hamburger.addEventListener('click', () => {
-            console.log("Coño");
-          mobileMenu.classList.toggle('active');
-        });
+    hamburger.addEventListener('click', () => {
+        mobileMenu.classList.toggle('active');
+    });
 
-   });
+
+});
